a60a4ae94bec8d8539d5f73a324f084a558bd018
Jose R <jose@joseramirez.dev>
2025-09-05 18:11:36 -0500

    theme save

--- assets/CWC_testimonial_images.js
+++ assets/CWC_testimonial_images.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".cwc-testimonial-images__carousel", {
     slidesPerView: 3,
-    spaceBetween: 10,
+    spaceBetween: 46,
     loop: true,
     speed: 400,
     navigation: {
